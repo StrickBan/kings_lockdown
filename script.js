ae15c93c1ec8098572e9aac0408fbd6f623bdc75
Luis <lecarcamol@gmail.com>
2022-07-23 20:20:39 -0300

    feat: Add song duration dynamically
    Added setMusic() and formatTime() functions to script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const bioButton = document.querySelector('#bioButton');
 const bio = document.querySelector('#bio');
-let music = document.querySelector('#audio');
+let music = document.querySelector('.audio');
 let seekBar = document.querySelector('.seek-bar');
 let currentTime = document.querySelector('.current-time');
 let musicDuration = document.querySelector('.song-duration');
@@ -16,3 +16,24 @@ bioButton.addEventListener('click', (e)=> {
 playBtn.addEventListener('click', () => {
   playBtn.classList.toggle('pause');
 })
+
+let setMusic = () => {
+  setTimeout(() => {
+    seekBar.max = music.duration;
+    musicDuration.innerHTML = formatTime(music.duration);
+  }, 300)
+}
+
+let formatTime = (time) => {
+  let min = Math.floor(time/60);
+  if(min < 10){
+    min = `0${min}`
+  }
+  let sec = Math.floor(time % 60);
+  if (sec < 10){
+    sec = `0${sec}`
+  }
+  return `${min}:${sec}`
+}
+
+setMusic()

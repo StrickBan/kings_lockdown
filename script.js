7c290a0df73d1672bdf6d16eb6e5d83fa17fec37
Luis <lecarcamol@gmail.com>
2022-07-23 20:07:14 -0300

    style/feat: Add custom audio player
    Created div with class audio-player on HTML, stylized it, and added a play/pause toggle transition on script.js

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-const bioButton = document.querySelector('#bioButton')
-const bio = document.querySelector('#bio')
+const bioButton = document.querySelector('#bioButton');
+const bio = document.querySelector('#bio');
+let music = document.querySelector('#audio');
+let seekBar = document.querySelector('.seek-bar');
+let currentTime = document.querySelector('.current-time');
+let musicDuration = document.querySelector('.song-duration');
+let playBtn = document.querySelector('.play-btn');
 
 bioButton.addEventListener('click', (e)=> {
   e.preventDefault();
   bio.scrollIntoView({
     behavior: "smooth"
   })
+})
+
+playBtn.addEventListener('click', () => {
+  playBtn.classList.toggle('pause');
 })
